25df77725897312d1675380c05a32caaa88bc8e3
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-03-21 19:29:15 +0300

    add missing cfg file

---
+++ pkg/cfg/cfg.go
@@ -0,0 +1,36 @@
+package cfg
+
+import (
+	"go.uber.org/zap"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"path/filepath"
+)
+
+var (
+	Cfg Config
+	logger *zap.Logger
+	err error
+)
+
+func init() {
+	logger, err = zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ParseConfig(configFilePath string) {
+	filename, _ := filepath.Abs(configFilePath)
+	yamlFile, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	err = yaml.Unmarshal(yamlFile, &Cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	logger.Info("successfully parsed cfg file", zap.Int("request_count", len(Cfg.Requests)))
+}
